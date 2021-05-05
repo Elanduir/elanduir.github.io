4a90e9f38777d5887e808d68c7216e4d249659f4
Fabian Heuberger <fabian.heuberger@students.fhnw.ch>
2021-05-05 21:08:27 +0200

    Update helloWorld.js

--- helloWorld.js
+++ helloWorld.js
@@ -1,17 +1,10 @@
-var xhr = new XMLHttpRequest();
-xhr.withCredentials = true;
-
-xhr.addEventListener("readystatechange", function() {
-  if(this.readyState === 4) {
-    console.log(this.responseText);
-  }
-});
-
-xhr.open("GET", "https://graph.instagram.com/17841403737404666/media?access_token=IGQVJYNXpBWlhNOEhSVklpcDVTY3UtSkxQLU5XSjFpUnY5ajg5ZAjVXd2RueDFTZAmt1ZA3Q3RG4zWHhIQlY5ajgydkxxbnhmOXJ3S1FYSlFFcEJQOWJPUzF4MGwzZAWNjejZAVY1lwNWdn&fields=id,timestamp");
-
-xhr.send();
-var test = xhr.response;
-
-console.log("Updated 21:04");
-test.forEach(element => console.log(element));
+var settings = {
+    "url": "https://graph.instagram.com/17841403737404666/media?access_token=IGQVJYNXpBWlhNOEhSVklpcDVTY3UtSkxQLU5XSjFpUnY5ajg5ZAjVXd2RueDFTZAmt1ZA3Q3RG4zWHhIQlY5ajgydkxxbnhmOXJ3S1FYSlFFcEJQOWJPUzF4MGwzZAWNjejZAVY1lwNWdn&fields=id,timestamp",
+    "method": "GET",
+    "timeout": 0,
+  };
+  
+  $.ajax(settings).done(function (response) {
+    console.log(response);
+  });
 
