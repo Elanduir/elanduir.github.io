e6efe803682f7c7025cc7c06d73827d35e77cdb4
Fabian Heuberger <fabian.heuberger@students.fhnw.ch>
2021-05-05 20:44:19 +0200

    Added Instagram GET request

--- helloWorld.js
+++ helloWorld.js
@@ -1,1 +1,14 @@
-alert('Hello');
+var xhr = new XMLHttpRequest();
+xhr.withCredentials = true;
+
+xhr.addEventListener("readystatechange", function() {
+  if(this.readyState === 4) {
+    console.log(this.responseText);
+  }
+});
+
+xhr.open("GET", "https://graph.instagram.com/17841403737404666/media?access_token=IGQVJYNXpBWlhNOEhSVklpcDVTY3UtSkxQLU5XSjFpUnY5ajg5ZAjVXd2RueDFTZAmt1ZA3Q3RG4zWHhIQlY5ajgydkxxbnhmOXJ3S1FYSlFFcEJQOWJPUzF4MGwzZAWNjejZAVY1lwNWdn&fields=id,timestamp");
+
+xhr.send();
+
+document.write(xhr.response)
