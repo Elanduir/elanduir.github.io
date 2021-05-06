ef2aabcbb9acced2b7eaf48359b6823bd4bf4894
Fabian Heuberger <fabian.heuberger@students.fhnw.ch>
2021-05-06 12:52:35 +0200

    updated script

--- helloWorld.js
+++ helloWorld.js
@@ -1,10 +1,35 @@
-var settings = {
+var test = [];
+
+function main(){
+  var settings = {
     "url": "https://graph.instagram.com/17841403737404666/media?access_token=IGQVJYNXpBWlhNOEhSVklpcDVTY3UtSkxQLU5XSjFpUnY5ajg5ZAjVXd2RueDFTZAmt1ZA3Q3RG4zWHhIQlY5ajgydkxxbnhmOXJ3S1FYSlFFcEJQOWJPUzF4MGwzZAWNjejZAVY1lwNWdn&fields=id,timestamp",
     "method": "GET",
+    "dataType": 'JSON',
     "timeout": 0,
   };
-  
+  let test = [];
   $.ajax(settings).done(function (response) {
-    console.log(response);
+    printResponse(response);
   });
+
+}
+
+
+
+function printResponse(res){
+  res.data.forEach(function(item, index, array){test.push(item)});
+  printArray(test);
+}
+
+function printArray(array){
+  console.log(array);
+}
+
+
+
+
+
+
+
+main();
 
